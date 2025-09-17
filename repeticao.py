81625d57f4cdb1386f8fd8a84f0ad91f15247731
Tiago <tiagoy924@gmail.com>
2025-09-17 20:16:17 -0300

    Trabalhando for em python

---
+++ repeticao.py
@@ -0,0 +1,26 @@
+"""
+for(etapa1, etapa2, etapa3):{
+
+}
+
+"""
+
+## Exemplo 1 - Contador de 1 a 10, não conta o valor final 10 vai até 9.
+for contador in range(1, 11):
+    print(contador, end=' ') 
+
+print("\n")
+print('-' * 20)
+
+## Exemplo 2 - Contador de 10 a 1, não conta o valor final 1 vai até 2.
+for contador in range(10, 0, -1):
+    print(contador, end=' ')
+# end=' ' -> Imprime na mesma linha, inicialmente o padrão é '\n' que pula a linha.
+
+print("\n")
+print('-' * 20)
+
+
+## Exemplo 3 - Contador de 1 a 10, pulando de 2 em 2.
+for contador in range(0, 22, 2):
+    print(contador, end=', ')
